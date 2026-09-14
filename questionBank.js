2d17412c407b3e1ea1a71e538850b5f1fa0e6738
mppscmcqhub <ajay.com1989@gmail.com>
2026-09-14 12:43:19 +0530

    Create questionBank.js

---
+++ questionBank.js
@@ -0,0 +1,34 @@
+window.questionBank = {
+  unit1: {
+    test1: [],
+    test2: [],
+    test3: [],
+    test4: [],
+    test5: [],
+    test6: [],
+    test7: [],
+    test8: [],
+    test9: [],
+    test10: [],
+    test11: [],
+    test12: [],
+    test13: [],
+    test14: [],
+    test15: [],
+    test16: [],
+    test17: [],
+    test18: [],
+    test19: [],
+    test20: []
+  },
+
+  unit2: {},
+  unit3: {},
+  unit4: {},
+  unit5: {},
+  unit6: {},
+  unit7: {},
+  unit8: {},
+  unit9: {},
+  unit10: {}
+};
